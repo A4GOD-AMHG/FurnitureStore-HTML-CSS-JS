618ed99fd08d883cc783ad32395a9b65eb011bd0
a4god- <alexismhgarcia@gmail.com>
2025-03-12 04:29:16 -0400

    header for mobile breakpoint done

--- js/script.js
+++ js/script.js
@@ -1,3 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.menu');
+    const navLinks = document.querySelector('.navbar-links');
+
+    menu.addEventListener('click', function (e) {
+        e.stopPropagation();
+        this.classList.toggle('active');
+        navLinks.classList.toggle('active');
+    });
+
+    navLinks.addEventListener('click', function (e) {
+        if (e.target === this) {
+            menu.classList.remove('active');
+            this.classList.remove('active');
+        }
+    });
+
+    document.addEventListener('click', function (e) {
+        if (!navLinks.contains(e.target) && !menu.contains(e.target)) {
+            menu.classList.remove('active');
+            navLinks.classList.remove('active');
+        }
+    });
+
+    document.querySelectorAll('.nav-link').forEach(link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            navLinks.classList.remove('active');
+        });
+    });
+});
+
 const observer = new IntersectionObserver(
     (entries) => {
         entries.forEach((entry) => {
